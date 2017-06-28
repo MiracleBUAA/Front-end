e180d6937f6fcef9df91355b2be894508b233311
husheng12345 <husheng_7@163.com>
2017-06-28 11:42:28 +0800

    添加了侧边栏选中特效。

---
+++ public/js/student_list.js
@@ -0,0 +1,11 @@
+/**
+ * Created by 胡晟 on 2017/6/28.
+ */
+
+$(document).ready(function () {
+  document.getElementById("course_info").className += " nav-active";
+  document.getElementById("student_list").className += " active";
+
+
+
+})
