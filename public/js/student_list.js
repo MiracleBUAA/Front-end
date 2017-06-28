a867d8c53fb71271f86c834cb288768c4e87037c
husheng12345 <husheng_7@163.com>
2017-06-28 15:18:06 +0800

    “上传学生列表”js

--- public/js/student_list.js
+++ public/js/student_list.js
@@ -2,10 +2,27 @@
  * Created by 胡晟 on 2017/6/28.
  */
 
+//  页面侧边栏选中
 $(document).ready(function () {
-  document.getElementById("course_info").className += " nav-active";
-  document.getElementById("student_list").className += " active";
-
-
-
+    document.getElementById("course_info").className += " nav-active";
+    document.getElementById("student_list").className += " active";
 })
+// 点击上传按钮
+$(document).ready(function(){
+    $("#upload_button").on("click",function(){
+        console.log("111");
+        var oFiles = document.querySelector("#upload_file").files;
+        var formData = new FormData();
+
+        for (var i = 0, file; file = oFiles[i]; i++) {
+            formData.append(file.name, file);
+        }
+
+        var xhr = new XMLHttpRequest();
+
+        xhr.open("POST", "/teacher/student_list", true);
+        // 发送表单数据
+        xhr.send(formData);
+
+    });
+});
