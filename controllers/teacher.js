fda21bc1228b01119913c013f661097b898f6165
stormspiritLiu <a912883181@163.com>
2017-06-28 10:45:40 +0800

    适配于测试的一个更改

--- controllers/teacher.js
+++ controllers/teacher.js
@@ -5,26 +5,56 @@ var express = require('express');
 var router =  express.Router();
 var url = require('url');
 var request = require('request');
+var fs = require('fs');
 
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'teacher' });
 });
 
 router.get('/student_list',function (req,res,next) {
-    var url = "http://localhost:8080/teacher/student_list"
-    request(url,function (error,response,body) {
-        var dataJson = eval("(" + body + ")");
-        if(!error && response.statusCode == 200){
-            res.render('student_list',{ students : dataJson });
-        }
-    })
+    console.log("enter");
+    res.render('teacher/student_list', { title: 'student_list' });
+    // var url = "http://localhost:8080/teacher/student_list"
+    // request(url,function (error,response,body) {
+    //     var dataJson = eval("(" + body + ")");
+    //     if(!error && response.statusCode == 200){
+    //         res.render('teacher/student_list',{ students : dataJson });
+    //     }
+    // })
 }).post('/student_list',function (req,res,next) {
     var url = "http://localhost:8080/teacher/student_list";
-    request.post({url:url, form:req.body.students}, function(error, response, body) {
+    var formData = {
+        file: fs.createReadStream(req.body.formData),
+    };
+    request.post({url:url, formdata:formData}, function(error, response, body) {
         if (!error && response.statusCode == 200) {
             res.redirect('/teacher/student_list');
         }
     })
+});
+
+router.get('/resource',function (req,res,next) {
+    var url = "http://localhost:8080/teacher/resource";
+    request(url,function (error,response,body) {
+        var dataJson = eval("(" + body + ")");
+        if(!error && response.statusCode == 200){
+            res.render('teacher/student_list',{ students : dataJson });
+        }
+    })
+}).post('/resource',function (req,res,next) {
+
+})
+
+router.get('/homework_list',function (req,res,next) {
+
+}).post('/homework_list',function (req,res,next) {
+
+})
+
+router.get('/homework_new',function (req,res,next) {
+
+}).post('/homework_new',function (req,res,next) {
+
 })
 
 module.exports = router;
