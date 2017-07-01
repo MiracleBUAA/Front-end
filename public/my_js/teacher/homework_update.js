14d824b6045562108b18423fbfd06488f42c0c58
husheng12345 <husheng_7@163.com>
2017-07-01 20:02:33 +0800

    添加“编辑作业要求”界面。

---
+++ public/my_js/teacher/homework_update.js
@@ -0,0 +1,14 @@
+/**
+ * Created by 胡晟 on 2017/7/1.
+ */
+
+
+//  页面侧边栏选中
+$(document).ready(function () {
+    document.getElementById("homework_li").className += " nav-active";
+    document.getElementById("homework_list_li").className += " active";
+});
+
+
+
+
