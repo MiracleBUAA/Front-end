19fbfe56ba11641581f87fedeecdc13f4a2684ac
husheng12345 <husheng_7@163.com>
2017-07-01 16:50:07 +0800

    又修改了文件名和路径。

--- public/myjs/teacher/student_list.js
+++ public/myjs/teacher/student_list.js
@@ -1,3 +1,15 @@
+/**
+ * Created by 胡晟 on 2017/6/28.
+ */
+
+//  页面侧边栏选中
+$(document).ready(function () {
+    document.getElementById("course_li").className += " nav-active";
+    document.getElementById("student_list_li").className += " active";
+});
+
+
+//表格初始化
 function fnFormatDetails ( oTable, nTr )
 {
     var aData = oTable.fnGetData( nTr );
@@ -13,7 +25,15 @@ function fnFormatDetails ( oTable, nTr )
 $(document).ready(function() {
 
     $('#dynamic-table').dataTable( {
-        "aaSorting": [[ 4, "desc" ]]
+        "aaSorting": [[ 4, "desc" ]],
+        "oLanguage": {
+            "sSearch": "检索：",
+            "sInfo": "_START_ ~ _END_ &nbsp;&nbsp;(共计_TOTAL_ 人)",
+            "sLengthMenu" : "每页 _MENU_ ",
+            "sZeroRecords": "没有匹配的结果",
+            "sInfoEmpty": "共计 0",
+            "sInfoFiltered": "",
+        },
     } );
 
     /*
@@ -62,3 +82,70 @@ $(document).ready(function() {
         }
     } );
 } );
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+
+    $("#upload_button").click(function (event) {
+
+        //stop submit the form, we will post it manually.
+        event.preventDefault();
+
+        fire_ajax_submit();
+
+    });
+
+});
+
+
+function fire_ajax_submit() {
+
+    // Get form
+    var form = $('#upload_form')[0];
+
+    var data = new FormData(form);
+
+
+    // for (var value11 of data.values()) {
+    //     console.log(value11);
+    // }
+
+
+
+    $("#upload_button").prop("disabled", true);
+
+    $.ajax({
+        type: "POST",
+        enctype: 'multipart/form-data',
+        //url直接通到黄老板
+        url:"http://localhost:8080/teacher/student_list?uid=1",
+        // url: "/teacher/student_list",
+        data: data,
+        //http://api.jquery.com/jQuery.ajax/
+        //http://developer.mozilla.org/en-US/docs/Web/API/FormData/Using_FormData_Objects
+        processData: false, //prevent jQuery from automatically transforming the data into a query string
+        contentType: false,
+        cache: false,
+        timeout: 600000,
+        success: function (data) {
+
+            console.log("SUCCESS : ", data);
+            $("#upload_button").prop("disabled", false);
+
+        },
+        error: function (e) {
+
+            console.log("ERROR : ", e);
+            $("#upload_button").prop("disabled", false);
+        }
+    });
+
+}
