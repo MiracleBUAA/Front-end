4c4ad54ad5c693a801967ad0e9e5ebff1f85e7f0
husheng12345 <husheng_7@163.com>
2017-07-02 14:38:12 +0800

    微调。

--- public/my_js/teacher/student_list.js
+++ public/my_js/teacher/student_list.js
@@ -35,6 +35,15 @@ $(document).ready(function() {
             "sInfoFiltered": "",
         },
     } );
+    $.ajax({
+        url : '/admin/student_list',
+        dataType : 'json',
+        type : 'post',
+        success : function (res) {
+            //alert(res.url);
+            URL = res.url;
+        }
+    })
 
     /*
      * Insert a 'details' column to the table
@@ -85,67 +94,3 @@ $(document).ready(function() {
 
 
 
-
-
-
-
-
-
-
-$(document).ready(function () {
-
-    $("#upload_button").click(function (event) {
-
-        //stop submit the form, we will post it manually.
-        event.preventDefault();
-
-        fire_ajax_submit();
-
-    });
-
-});
-
-
-function fire_ajax_submit() {
-
-    // Get form
-    var form = $('#upload_form')[0];
-
-    var data = new FormData(form);
-
-
-    // for (var value11 of data.values()) {
-    //     console.log(value11);
-    // }
-
-
-
-    $("#upload_button").prop("disabled", true);
-
-    $.ajax({
-        type: "POST",
-        enctype: 'multipart/form-data',
-        //url直接通到黄老板
-        url:"http://localhost:8080/teacher/student_list?uid=1",
-        // url: "/teacher/student_list",
-        data: data,
-        //http://api.jquery.com/jQuery.ajax/
-        //http://developer.mozilla.org/en-US/docs/Web/API/FormData/Using_FormData_Objects
-        processData: false, //prevent jQuery from automatically transforming the data into a query string
-        contentType: false,
-        cache: false,
-        timeout: 600000,
-        success: function (data) {
-
-            console.log("SUCCESS : ", data);
-            $("#upload_button").prop("disabled", false);
-
-        },
-        error: function (e) {
-
-            console.log("ERROR : ", e);
-            $("#upload_button").prop("disabled", false);
-        }
-    });
-
-}
