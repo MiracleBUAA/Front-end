00b49a67ed40e208da4c7c1d65892d4bae0889e3
husheng12345 <husheng_7@163.com>
2017-07-03 14:15:38 +0800

    修改了“学生创建团队”界面。

---
+++ public/my_js/student/mygroup_homeless.js
@@ -0,0 +1,11 @@
+/**
+ * Created by 胡晟 on 2017/7/3.
+ */
+
+
+
+// 页面侧边栏选中
+$(document).ready(function () {
+    document.getElementById("group_li").className += " nav-active";
+    document.getElementById("mygroup_li").className += " active";
+});
