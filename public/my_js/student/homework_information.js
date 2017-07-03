0afd6a8ee2f96c1339a9797cb49d3fe63f6ab4de
zhufu <snbjzf@163.com>
2017-07-03 10:08:23 +0800

    学生作业上传ajax

--- public/my_js/student/homework_information.js
+++ public/my_js/student/homework_information.js
@@ -24,13 +24,21 @@ $(document).ready(function () {
 
 });
 
+var file_name;
+function getFileName(s){
+     var pos=s.lastIndexOf("\\");
+     return s.substring(pos+1);
+}
+document.getElementById("upload_file").onchange=function () {
+    file_name = getFileName(this.value);
+}
 
 function fire_ajax_submit() {
 
     // Get form
     var form = $('#upload_form')[0];
 
-    var data = new FormData(form);
+    var file_data = new FormData(form);
 
 
     // for (var value11 of data.values()) {
@@ -38,9 +46,45 @@ function fire_ajax_submit() {
     // }
 
 
-
     $("#upload_button").prop("disabled", true);
 
+    var curID = Number(document.getElementById("upload_form").getAttribute("data-homework-id"));
+
+    $.ajax({
+        type: 'post',
+        dataType: 'json',
+        url: '/student/homework_upload',
+        data: {
+            title: file_name,
+            homework_id: curID
+        },
+        error: function () {
+            alert("请求url错误！");
+        },
+        success: function (res) {
+            alert(res.url);
+            $.ajax({
+                type: 'post',
+                enctype: 'multipart/form-data',
+                url: res.url,
+                data: file_data,
+                processData: false,
+                contentType: false,
+                cache: false,
+                timeout: 600000,
+                success: function (data) {
+                    console.log("SUCESS :", data);
+                    window.location.href = "/student/homework_information";
+                },
+                error: function (e) {
+                    console.log("ERROR :", e);
+                    window.location.href = '/student/homework_information';
+                }
+            });
+        }
+    });
+}
+    /*
     $.ajax({
         type: "POST",
         enctype: 'multipart/form-data',
@@ -66,5 +110,4 @@ function fire_ajax_submit() {
             window.location.href = "/student/homework_information";
         }
     });
-
-}
+    */
