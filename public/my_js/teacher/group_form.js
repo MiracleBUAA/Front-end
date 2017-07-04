0ba2b9c6e4d2674590fae5b3e6b24ff9170a0cb7
zhufu <snbjzf@163.com>
2017-07-04 10:56:38 +0800

    下载按钮

--- public/my_js/teacher/group_form.js
+++ public/my_js/teacher/group_form.js
@@ -84,3 +84,17 @@ $(document).ready(function() {
         }
     } );
 } );
+
+function downgroup() {
+    $.ajax({
+        url:'/teacher/group_form_download',
+        type:'get',
+        dataType:'json',
+        error:function () {
+            alert('ERROR group form');
+        },
+        success:function () {
+            ;
+        }
+    })
+}
