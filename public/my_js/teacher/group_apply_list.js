435e8e0e069bb1866ea6f392407db86e675ec510
zhufu <snbjzf@163.com>
2017-07-04 09:57:51 +0800

    按钮事件

--- public/my_js/teacher/group_apply_list.js
+++ public/my_js/teacher/group_apply_list.js
@@ -13,3 +13,40 @@ $(document).ready(function () {
 $('.reject-in').click(function () {
     $(this).parents(".panel").parent().remove();
 });
+
+function accept(e) {
+    var curID = Number(e.getAttribute("data-id"));
+    
+    $.ajax({
+        url:'teacher/group_confirm',
+        type:'post',
+        dataType:'json',
+        data:{
+            group_apply_id:curID
+        },
+        error:function () {
+            alert("ERROR accept");
+        },
+        success:function () {
+
+        }
+    })
+}
+function reject(e) {
+    var curID = Number(e.getAttribute("data-id"));
+
+    $.ajax({
+        url:'teacher/group_reject',
+        type:'post',
+        dataType:'json',
+        data:{
+            group_apply_id:curID
+        },
+        error:function () {
+            alert("ERROR reject");
+        },
+        success:function () {
+
+        }
+    })
+}
