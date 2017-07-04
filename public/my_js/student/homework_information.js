2e6586e95f31d06202b7e25f62c23f9d12226e38
husheng12345 <husheng_7@163.com>
2017-07-05 02:25:08 +0800

    添加了约束：作业到期后，不能上传也不能删除文件。

--- public/my_js/student/homework_information.js
+++ public/my_js/student/homework_information.js
@@ -22,6 +22,23 @@ $(document).ready(function () {
 
     });
 
+
+    var _end=$("#end").html();
+    var end_time=new Date(_end);
+    var now = new Date().getTime();
+
+    if (now-end_time>0){
+        $("#end").html(_end+"<font color='red'> (已结束)</font>");
+        $("#upl").html('');
+        $('.dele').prop("disabled", true);
+
+    }
+    else
+        $("#end").html(_end+"<font color='green'> (进行中)</font>");
+
+
+
+
 });
 
 var file_name;
@@ -86,3 +103,45 @@ function fire_ajax_submit() {
         }
     });
 }
+
+
+
+
+
+//JavaScript函数：
+var minute = 1000 * 60;
+var hour = minute * 60;
+var day = hour * 24;
+var halfamonth = day * 15;
+var month = day * 30;
+function getDateDiff(dateTimeStamp){
+    var now = new Date().getTime();
+    var diffValue = now - dateTimeStamp;
+    if(diffValue < 0){
+        //若日期不符则弹出窗口告之
+        //alert("结束日期不能小于开始日期！");
+    }
+    var monthC =diffValue/month;
+    var weekC =diffValue/(7*day);
+    var dayC =diffValue/day;
+    var hourC =diffValue/hour;
+    var minC =diffValue/minute;
+    if(monthC>=1){
+        result="发布于" + parseInt(monthC) + "个月前";
+    }
+    else if(weekC>=1){
+        result="发布于" + parseInt(weekC) + "周前";
+    }
+    else if(dayC>=1){
+        result="发布于"+ parseInt(dayC) +"天前";
+    }
+    else if(hourC>=1){
+        result="发布于"+ parseInt(hourC) +"个小时前";
+    }
+    else if(minC>=1){
+        result="发布于"+ parseInt(minC) +"分钟前";
+    }else
+        result="刚刚发布";
+    return result;
+}
+
