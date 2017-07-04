649ba524a26da566fa49c8055596782dde0c38c1
zhufu <snbjzf@163.com>
2017-07-04 15:27:30 +0800

    组内评分

--- public/my_js/student/group_rate.js
+++ public/my_js/student/group_rate.js
@@ -10,3 +10,26 @@ $(document).ready(function () {
 
 //spinner
 $('.spinner-s').spinner({value: 10, step: 2, min: 4, max: 12});
+
+function send(e) {
+    var curID = e.getAttribute("data-id");
+    var score = $("input[data-rec="+curID+"]").val();
+    curID = Number(curID);
+    score = Number(score)/10;
+
+    $.ajax({
+        url:'/student/group_rate',
+        type:'post',
+        dataType:'json',
+        data:{
+            student_id:curID,
+            student_rate:score
+        },
+        error:function () {
+            alert("ERROR group rate!");
+        },
+        success:function () {
+            ;
+        }
+    })
+}
