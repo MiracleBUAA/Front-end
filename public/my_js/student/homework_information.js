df135ca1fc42b3fd11878c8ba9d9eed07f81fda0
husheng12345 <husheng_7@163.com>
2017-07-05 03:14:15 +0800

    添加了约束：发布新作业时，所有字段必填，并且时间有前后。

--- public/my_js/student/homework_information.js
+++ public/my_js/student/homework_information.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
 
     });
 
-
+    // 时间约束
     var _end=$("#end").html();
     var end_time=new Date(_end);
     var now = new Date().getTime();
@@ -105,43 +105,4 @@ function fire_ajax_submit() {
 }
 
 
-
-
-
-//JavaScript函数：
-var minute = 1000 * 60;
-var hour = minute * 60;
-var day = hour * 24;
-var halfamonth = day * 15;
-var month = day * 30;
-function getDateDiff(dateTimeStamp){
-    var now = new Date().getTime();
-    var diffValue = now - dateTimeStamp;
-    if(diffValue < 0){
-        //若日期不符则弹出窗口告之
-        //alert("结束日期不能小于开始日期！");
-    }
-    var monthC =diffValue/month;
-    var weekC =diffValue/(7*day);
-    var dayC =diffValue/day;
-    var hourC =diffValue/hour;
-    var minC =diffValue/minute;
-    if(monthC>=1){
-        result="发布于" + parseInt(monthC) + "个月前";
-    }
-    else if(weekC>=1){
-        result="发布于" + parseInt(weekC) + "周前";
-    }
-    else if(dayC>=1){
-        result="发布于"+ parseInt(dayC) +"天前";
-    }
-    else if(hourC>=1){
-        result="发布于"+ parseInt(hourC) +"个小时前";
-    }
-    else if(minC>=1){
-        result="发布于"+ parseInt(minC) +"分钟前";
-    }else
-        result="刚刚发布";
-    return result;
-}
 
--- public/my_js/teacher/new_homework.js
+++ public/my_js/teacher/new_homework.js
@@ -15,3 +15,24 @@ $(document).ready(function () {
 $(".form_datetime-component").datetimepicker({
     format: "yyyy-mm-dd hh:ii:ss"
 });
+
+
+// 时间约束
+function OnChange () {
+
+    var _start = $("#start").val();
+    var _end=$('#end').val();
+    var start_time=new Date(_start);
+    var end_time=new Date(_end);
+    if (start_time-end_time>0)
+    {
+        alert("截止时间不能早于开始时间！");
+        $('#sub').prop("disabled", true);
+        $('#sub').html("时间错误，请修改");
+    }
+    else{
+        $('#sub').prop("disabled", false);
+        $('#sub').html("确认发布");
+    }
+}
+
