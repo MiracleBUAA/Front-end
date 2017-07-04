54af5862fa537d9112fa45cb21e3ae0a510ca4bf
husheng12345 <husheng_7@163.com>
2017-07-04 11:41:21 +0800

    添加了“组内评分”页面，还没完成。

---
+++ public/my_js/student/group_rate.js
@@ -0,0 +1,9 @@
+/**
+ * Created by 胡晟 on 2017/7/4.
+ */
+
+//  页面侧边栏选中
+$(document).ready(function () {
+    document.getElementById("group_li").className += " nav-active";
+    document.getElementById("rate_li").className += " active";
+});
